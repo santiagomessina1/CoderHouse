220d243a3ef2b4623ccdb54db2cf1425b52528b0
santimessina <santiagomessina1@gmail.com>
2022-07-02 13:48:07 -0300

    First commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// CONTADOR DE 5 EN 5 HASTA EL 50
+
+let numIngresado = parseInt(prompt("Ingrese un numero menor a 50 por favor: "))
+
+
+    for (contador = numIngresado; contador <= 150; contador = contador + 5){
+
+        if(contador != numIngresado){
+            alert(contador)
+        }
+      
+    }
